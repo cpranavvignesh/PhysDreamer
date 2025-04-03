e50f2fcb2e56d08b73386ec50d4a5564d08d2abb
C Pranav Vignesh <95594858+cpranavvignesh@users.noreply.github.com>
2025-04-03 15:36:32 +0530

    commented out commands for telephone

--- projects/inference/run.sh
+++ projects/inference/run.sh
@@ -2,6 +2,6 @@
 
 # python3 demo.py --scene_name hat --apply_force --force_id 0  --point_id 0 --force_mag 3.0 --cam_id 0
 
-python3 demo.py --scene_name telephone --apply_force --force_id 0  --point_id 0 --force_mag 0.1 --cam_id 0
+# python3 demo.py --scene_name telephone --apply_force --force_id 0  --point_id 0 --force_mag 0.1 --cam_id 0
 
 python3 demo.py --scene_name alocasia --apply_force --force_id 0 --point_id 0 --force_mag 3.0 --cam_id 0
